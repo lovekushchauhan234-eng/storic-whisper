126f2ed1148f14357c5b14bdc84063eaf1c23299
Lovekush Chauhan <lovekushchauhan234@gmail.com>
2026-05-27 15:22:34 +0530

    update admin editor

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Article, Subscriber
 
 
+from django.db import models
+from django.forms import Textarea
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display  = ('title', 'category', 'is_published', 'is_featured', 'created_at')
@@ -9,7 +12,9 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
     list_editable = ('is_published', 'is_featured')
-
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 30, 'cols': 100})},
+    }
 
 @admin.register(Subscriber)
 class SubscriberAdmin(admin.ModelAdmin):
