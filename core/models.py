lovekushchauhan234-eng/storic-whisper

commit 18eaf60e674acad31b78f951c71ef821dd133e7a

Remove duplicate old english_hub.html template file

core/models.py
@@ -65,8 +65,7 @@ class Article(models.Model):
     thumbnail = models.ImageField(
         upload_to='thumbs/',
         blank=True,
-        null=True,
-        storage=MediaCloudinaryStorage()
+        null=True
     )
 
     content = RichTextField()

storic_whisper_site/core/models.py
@@ -65,8 +65,7 @@ class Article(models.Model):
     thumbnail = models.ImageField(
         upload_to='thumbs/',
         blank=True,
-        null=True,
-        storage=MediaCloudinaryStorage()
+        null=True
     )
 
     content = RichTextField()

commit fad2bf33d34585e334d4b1a5d86d13a25bbd0399

EMERGENCY FIX: Restore MediaCloudinaryStorage to thumbnail field - regression from commit 18eaf60

core/models.py
@@ -65,7 +65,8 @@ class Article(models.Model):
     thumbnail = models.ImageField(
         upload_to='thumbs/',
         blank=True,
-        null=True
+        null=True,
+        storage=MediaCloudinaryStorage()
     )
 
     content = RichTextField()

storic_whisper_site/core/models.py
@@ -65,7 +65,8 @@ class Article(models.Model):
     thumbnail = models.ImageField(
         upload_to='thumbs/',
         blank=True,
-        null=True
+        null=True,
+        storage=MediaCloudinaryStorage()
     )
 
     content = RichTextField()